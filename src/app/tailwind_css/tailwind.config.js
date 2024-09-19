da382c5ba2848680900d1c33455ebddc9b1c98ea
username <user_khirade36@gmal.com>
2024-09-20 02:27:47 +0530

    Project initialization and initail app setup for next js and tailwind css

---
+++ src/app/tailwind_css/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+
+};
---
+++ src/store/useTaskStore.js
@@ -0,0 +1,7 @@
+import create from 'zustand';
+
+const useTaskStore = create(Set => ({
+    
+})
+
+)
