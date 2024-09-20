f247a15a23b2152dc79589a4a5817a600758bd6a
username <user_khirade36@gmal.com>
2024-09-21 03:36:37 +0530

    Add Usetaskstore function

--- src/store/useTaskStore.js
+++ src/store/useTaskStore.js
@@ -1,7 +1,16 @@
 import create from 'zustand';
 
 const useTaskStore = create(Set => ({
-    
-})
+    task:[],
+    addTask: (task) => Set(state => ({task: [...state.task, task]})),
+    updateTask: (id , task) => Set(state =>({
+        task: state.task.map(t => t.id === id ? task : t)
+    })),
 
-)
+    deleteTask: id => Set(state => ({
+        task: state.task.filter(t => t.id !== id)
+    })),    
+}));
+    
+
+export default useTaskStore;
